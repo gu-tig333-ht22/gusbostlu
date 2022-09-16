064810c658bd7818c60d8acc1219e86bbb46b0f9
luddluddelito <ludwig.bostrom69@gmail.com>
2022-09-16 11:56:30 +0200

    Final inläming vecka 3

---
+++ lib/ToDoWidget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/dataToDo.dart';
+import 'addToDoView.dart';
+
+import 'dataToDo.dart';
+
+class ToDoWidget extends StatelessWidget {
+  final ToDo thingtodo;
+
+  ToDoWidget(
+    this.thingtodo,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text(thingtodo.getMessage));
+  }
+}
---
+++ lib/filterList.dart
@@ -0,0 +1,10 @@
+import 'package:my_app/main.dart';
+
+/*List<itemList>? _filterList(list, filterBy) {
+    if (filterBy == "All") 
+    return list;
+    if (filterBy ==  "Done") 
+    list.where((item)) => item.value == true).tolist();
+    if (filterBy ==  "NotDone") 
+    list.where((item)) => item.value == fals).tolist();
+    return null;*/
